90cdfe0bd36d681edbad395b907fb7ba3200bf2d
randelsouza <randelsouza88@gmail.com>
2021-01-20 14:11:02 -0300

    added delete mapping resource LancamentoResouce.java

--- src/main/java/com/example/financialdashboard/api/resource/LancamentoResource.java
+++ src/main/java/com/example/financialdashboard/api/resource/LancamentoResource.java
@@ -9,12 +9,15 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.financialdashboard.api.event.RecursoCriadoEvent;
@@ -62,6 +65,12 @@ public class LancamentoResource {
 	public ResponseEntity<?> buscarPeloCodio(@PathVariable Long codigo) {
 		Optional<Lancamento> lancamento = this.lancamentoRepository.findById(codigo);		
 		return lancamento.isPresent() ? ResponseEntity.ok(lancamento.get()) : ResponseEntity.notFound().build(); 
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletarLancamentoPeloCodigo(@PathVariable Long codigo) {
+		this.lancamentoRepository.deleteById(codigo);
 	}
 	
 	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
