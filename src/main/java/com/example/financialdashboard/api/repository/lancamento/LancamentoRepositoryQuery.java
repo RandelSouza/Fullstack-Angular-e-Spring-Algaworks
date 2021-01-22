d09204a0f5277d6d9f3f663722b206c7d5b9d352
randelsouza <randelsouza88@gmail.com>
2021-01-21 23:11:48 -0300

    added one comment

--- src/main/java/com/example/financialdashboard/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/financialdashboard/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,14 @@
 package com.example.financialdashboard.api.repository.lancamento;
 
-import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.example.financialdashboard.api.model.Lancamento;
 import com.example.financialdashboard.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
